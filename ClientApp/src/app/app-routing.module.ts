4ff24c2119050070884c638841fcc1ec10ff28b9
Thiago Andrade Ferreira <thiagoandrade160302@gmail.com>
2023-11-24 16:03:24 -0300

    Proteção Rotas ath.guard

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,21 +2,21 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { AppLayoutComponent } from "./layout/app.layout.component";
+import { AuthGuard } from './auth.guard'; // Ajuste o caminho conforme necessário
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             {
-                path: '', component: AppLayoutComponent,
+                path: '', component: AppLayoutComponent, canActivate: [AuthGuard],
                 children: [
-                    { path: '', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'uikit', loadChildren: () => import('./components/uikit/uikit.module').then(m => m.UIkitModule) },
+                    { path: '', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule), canActivateChild: [AuthGuard] },
+                    { path: 'uikit', loadChildren: () => import('./components/uikit/uikit.module').then(m => m.UIkitModule), canActivateChild: [AuthGuard] },
                 ]
             },
-            // { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'landing', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
-            { path: 'cadastro', loadChildren: () => import('./components/cadastro/cadastro.module').then(m => m.CadastroModule) },
+            { path: 'login', canActivate: [AuthGuard], loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
+            { path: 'cadastro', canActivate: [AuthGuard], loadChildren: () => import('./components/cadastro/cadastro.module').then(m => m.CadastroModule) },
             { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
